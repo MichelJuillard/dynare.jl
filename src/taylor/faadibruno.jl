6d3b3112434745ba50b0c145a3c31295dcfe4e3b
Michel Juillard <michel@michel-zen3.home>
2017-08-16 13:49:43 +0200

    add Faa Di Bruno formula

---
+++ src/taylor/faadibruno.jl
@@ -0,0 +1,301 @@
+module FaaDiBruno
+using Combinatorics
+import Base: start, next, done
+include("../linalg/kronecker_utils.jl")
+import .Kronecker: a_mul_kron_b!
+
+export FaaDiBrunoWs, faa_di_bruno
+
+ttuple = Tuple{ Array{Int,1}, Array{Array{Int,1},1} }
+tatuple = Array{ttuple,1}
+trecipees = Array{Array{tatuple,1},1}
+
+struct FaaDiBrunoWs
+    recipees::Array{Array{tatuple,1},1}
+    work1::Vector{Float64}
+    work2::Vector{Float64}
+    matrixvector::Array{AbstractArray,1}
+    function FaaDiBrunoWs(neq,nvar,recipees,order)
+        setup_recipees!(recipees,order)
+        work1 = Vector{Float64}(neq*nvar^order)
+        work2 = Vector{Float64}(neq*nvar^order)
+        new(recipees,work1,work2)
+    end
+end
+
+function FaaDiBrunoWs(neq,nvar,order)
+    recipees = Array{Array{tatuple,1},1}(order)
+    FaaDiBrunoWs(neq,nvar,recipees,order)
+end
+
+multicat(a::Array{Array{Int64,1},1}) =  [a[i][j] for i in eachindex(a) for j in eachindex(a[i])]
+                  
+function setup_recipees!(recipees::trecipees,order::Int)
+    for i in 1:order
+        r1 = Array{tatuple,1}(0)
+        p1 = integer_partitions(i)
+        for j in 1:i
+            r2 = Array{tatuple,1}(0)
+            c = tatuple(0)
+            p2 = collect(partitions(collect(1:i),j))
+            map!(x -> sort(x,by=length),p2,p2)
+            patterns = map(x->map(y->length(y),x),p2)
+            p1j = filter(x->length(x) == j,p1)
+            for k = 1:length(p1j)
+                p1jk = p1j[k]
+                r2 = Array{Array{Int,1},1}(0)
+                for m = 1:length(p2)
+                    if patterns[m] == sort(p1jk)
+                        v = multicat(p2[m])
+                        push!(r2,ipermute!(collect(1:length(v)),v))
+                    end
+                end
+                push!(c,(p1jk,r2))
+            end
+            push!(r1,c)
+        end
+        recipees[i] = r1
+    end
+end
+
+function faa_di_bruno!(dfg::Matrix{Float64},f::Array{Matrix{Float64}},g::Array{Matrix{Float64}},order::Int,ws::FaaDiBrunoWs)
+    m = size(f[1],1)
+    n = size(g[1],2)
+    work1 = reshape(view(ws.work1,1:m*n^order),m,n^order)
+    if order == 1
+        A_mul_B!(dfg,f[1],g[1])
+    elseif order == 2
+        A_mul_B!(dfg,f[1],g[2])
+        a_mul_kron_b!(work1,f[2],g[1],2)
+        dfg .+= work1
+    else
+        for i = 1:order
+            if i == 1 && g[order] != [0]
+                A_mul_B!(dfg,f[1],g[order])
+                println(dfg[1])
+#            elseif i == order
+#                a_mul_kron_b!(work1,f[i],g[1],i)
+#                dfg .+= work1
+#                println(work1[1])
+#                println(dfg[1])
+            else
+                apply_recipees!(dfg,ws.recipees[order][i],f[i],g,order,ws)
+                println(dfg[1])
+            end
+        end
+    end
+end
+
+function apply_recipees!(dfg::Matrix{Float64},recipees::tatuple,f::AbstractArray,g::Array{Array{Float64,2},1},order::Int64,ws::FaaDiBrunoWs)
+    m = size(f,1)
+    n = size(g[1],2)
+    dims = (m, repmat([n],order)...)
+    work1 = reshape(view(ws.work1,1:m*n^order),m,n^order)
+    for i = 1:length(recipees)
+        recipees1 = recipees[i][1]
+        recipees2 = recipees[i][2]
+        fill!(work1,0.0)
+        fill!(ws.work2,0.0)
+        a_mul_kron_b!(work1,f,g[recipees1],ws.work2)
+        println("apply_recipees!.work1")
+        println(work1[1])
+        println(recipees1)
+        a = reshape(work1, dims)
+        d = reshape(dfg, dims)
+        p = length(recipees2)
+        matrixvector = Array{AbstractArray}(p)
+        dims0 = (1,recipees2[1]+1...)
+        for j = 1:length(recipees2)
+            dims1 = (1,recipees2[j]+1...)
+            matrixvector[j] = PermutedDimsArray(a,dims1)
+        end
+        println("dfg[1]")
+        println(dfg[1])
+        println([x[1] for x in matrixvector]) 
+        add!(d,matrixvector)
+        println(dfg[1])
+    end
+end
+
+"""
+    function add!(b::Matrix{Float64},a::Array{AbstractArray,1})
+adds to matrix b the sum of the (permuted) matrices in vector of matrices a
+
+The inside loop is unfolded up to 15 matrices
+"""
+function add!(b::AbstractArray{Float64},a::Array{AbstractArray,1})
+    n = length(a)
+    @inbounds b .+= a[1]
+    if n == 1
+        return
+    elseif n == 2
+        @inbounds b .+= a[2]
+    elseif n == 3
+        a2 = a[2]
+        a3 = a[3]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i]
+        end
+    elseif n == 4
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i]
+        end
+    elseif n == 5
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i]
+        end
+    elseif n == 6
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i]
+        end
+    elseif n == 7
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        a7 = a[7]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i] + a7[i]
+        end
+    elseif n == 8
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        a7 = a[7]
+        a8 = a[8]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i] + a7[i] + a8[i]
+        end
+    elseif n == 9
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        a7 = a[7]
+        a8 = a[8]
+        a9 = a[9]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i] + a7[i] + a8[i] + a9[i]
+        end
+    elseif n == 10
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        a7 = a[7]
+        a8 = a[8]
+        a9 = a[9]
+        a10 = a[10]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i] + a7[i] + a8[i] + a9[i] + a10[i]
+        end
+    elseif n == 11
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        a7 = a[7]
+        a8 = a[8]
+        a9 = a[9]
+        a10 = a[10]
+        a11 = a[11]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i] + a7[i] + a8[i] + a9[i] + a10[i] + a11[i]
+        end
+    elseif n == 12
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        a7 = a[7]
+        a8 = a[8]
+        a9 = a[9]
+        a10 = a[10]
+        a11 = a[11]
+        a12 = a[12]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i] + a7[i] + a8[i] + a9[i] + a10[i] + a11[i] + a12[i]
+        end
+    elseif n == 13
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        a7 = a[7]
+        a8 = a[8]
+        a9 = a[9]
+        a10 = a[10]
+        a11 = a[11]
+        a12 = a[12]
+        a13 = a[13]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i] + a7[i] + a8[i] + a9[i] + a10[i] + a11[i] + a12[i] + a13[i]
+        end
+    elseif n == 14
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        a7 = a[7]
+        a8 = a[8]
+        a9 = a[9]
+        a10 = a[10]
+        a11 = a[11]
+        a12 = a[12]
+        a13 = a[13]
+        a14 = a[14]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i] + a7[i] + a8[i] + a9[i] + a10[i] + a11[i] + a12[i] + a13[i] + a14[i]
+        end
+    elseif n == 15
+        a2 = a[2]
+        a3 = a[3]
+        a4 = a[4]
+        a5 = a[5]
+        a6 = a[6]
+        a7 = a[7]
+        a8 = a[8]
+        a9 = a[9]
+        a10 = a[10]
+        a11 = a[11]
+        a12 = a[12]
+        a13 = a[13]
+        a14 = a[14]
+        a15 = a[15]
+        @simd for i in CartesianRange(size(b))
+            @inbounds b[i] += a2[i] + a3[i] + a4[i] + a5[i] + a6[i] + a7[i] + a8[i] + a9[i] + a10[i] + a11[i] + a12[i] + a13[i] + a14[i] + a15[i]
+        end
+    else
+        na = length(a)
+        @inbounds for i in CartesianRange(size(b))
+            @simd for j = 2:na
+                b[i] += a[j][i]
+            end
+        end
+    end
+end
+              
+        
+end
+
---
+++ tests/taylor/test_faa_di_bruno.jl
@@ -0,0 +1,123 @@
+using ForwardDiff
+using Base.Test
+using TensorOperations
+
+function f(x)
+    [exp(x[1])*exp(2*x[2]), 2*exp(x[1])*exp(x[2])]
+end
+
+function g(x)
+    [x[1]^4*x[2]^3, x[1]^2*x[2]]
+end
+
+fg(x) = f(g(x))
+
+
+df1(x) = ForwardDiff.jacobian(f,x)
+df2(x) = ForwardDiff.jacobian(df1,x)
+df3(x) = ForwardDiff.jacobian(df2,x)
+df4(x) = ForwardDiff.jacobian(df3,x)
+dg1(x) = ForwardDiff.jacobian(g,x)
+dg2(x) = ForwardDiff.jacobian(dg1,x)
+dg3(x) = ForwardDiff.jacobian(dg2,x)
+dg4(x) = ForwardDiff.jacobian(dg3,x)
+dfg1(x) = ForwardDiff.jacobian(fg,x)
+dfg2(x) = ForwardDiff.jacobian(dfg1,x)
+dfg3(x) = ForwardDiff.jacobian(dfg2,x)
+dfg4(x) = ForwardDiff.jacobian(dfg3,x)
+
+include("../../src/taylor/faadibruno.jl")
+using FaaDiBruno
+faadibruno_ws = FaaDiBrunoWs(2,2,3)
+println(faadibruno_ws.recipees[3][2])
+faadibruno_ws = FaaDiBrunoWs(2,2,4)
+println(faadibruno_ws.recipees[3][2])
+
+
+n = 2
+dfg=Matrix{Float64}(n,n^3)
+x = randn(n)
+ff=[ df1(g(x)), reshape(df2(g(x)),n,n*n), reshape(df3(g(x)),n,n^3), reshape(df4(g(x)),n,n^4)]
+gg=[ dg1(x), reshape(dg2(x),n,n*n), reshape(dg3(x),n,n^3), reshape(dg4(x),n,n^4)]
+
+
+FaaDiBruno.apply_recipees!(dfg,faadibruno_ws.recipees[3][2],ff[2],gg,3,faadibruno_ws)
+t1 = ff[2]*kron(gg[2],gg[1])
+t2 = t1 + t1[:,[1, 3, 2, 4, 5, 7, 6, 8]] + t1[:,[1, 3, 5, 7, 2, 4, 6, 8]]
+@test dfg ≈ t2
+t3 = zeros(2,2,2,2)
+ff1 = reshape(ff[1],2,2)
+ff2 = reshape(ff[2],2,2,2)
+ff3 = reshape(ff[3],2,2,2,2)
+ff4 = reshape(ff[4],2,2,2,2,2)
+gg1 = reshape(gg[1],2,2)
+gg2 = reshape(gg[2],2,2,2)
+gg3 = reshape(gg[3],2,2,2,2)
+gg4 = reshape(gg[4],2,2,2,2,2)
+
+@tensor t3[a,b,c,d] = ff2[a,e,f]*gg2[e,b,c]*gg1[f,d] + ff2[a,e,f]*gg2[e,b,d]*gg1[f,c] + ff2[a,e,f]*gg2[e,d,c]*gg1[f,b]
+    
+@test dfg ≈ reshape(t3,2,8)
+
+t10 = zeros(2,2,2,2,2)
+t20 = zeros(2,2,2,2,2)
+t30 = zeros(2,2,2,2,2)
+t30a = zeros(2,2,2,2,2)
+t30b = zeros(2,2,2,2,2)
+t30c = zeros(2,2,2,2,2)
+t30d = zeros(2,2,2,2,2)
+t30e = zeros(2,2,2,2,2)
+t30f = zeros(2,2,2,2,2)
+t40 = zeros(2,2,2,2,2)
+
+@tensor begin
+    t10[a,b,c,d,e] = ff1[a,f]*gg4[f,b,c,d,e]
+    t20[a,b,c,d,e] = ff2[a,f,g]*gg2[f,b,c]*gg2[g,d,e] + ff2[a,f,g]*gg2[f,b,d]*gg2[g,c,e] + ff2[a,f,g]*gg2[f,b,e]*gg2[g,d,c] +
+        ff2[a,f,g]*gg3[f,b,c,d]*gg1[g,e] +  ff2[a,f,g]*gg3[f,b,c,e]*gg1[g,d] +  ff2[a,f,g]*gg3[f,b,e,d]*gg1[g,c] +  ff2[a,f,g]*gg3[f,e,c,d]*gg1[g,b]
+    t30[a,b,c,d,e] = ff3[a,f,g,h]*gg2[f,b,c]*gg1[g,d]*gg1[h,e] + ff3[a,f,g,h]*gg2[f,b,d]*gg1[g,c]*gg1[h,e] + ff3[a,f,g,h]*gg2[f,b,e]*gg1[g,d]*gg1[h,c] +
+        ff3[a,f,g,h]*gg2[f,c,d]*gg1[g,b]*gg1[h,e] +ff3[a,f,g,h]*gg2[f,c,e]*gg1[g,d]*gg1[h,b] +ff3[a,f,g,h]*gg2[f,d,e]*gg1[g,b]*gg1[h,c]
+    t30a[a,b,c,d,e] = ff3[a,f,g,h]*gg2[f,b,c]*gg1[g,d]*gg1[h,e]
+    t30b[a,b,c,d,e] = ff3[a,f,g,h]*gg2[f,b,d]*gg1[g,c]*gg1[h,e]
+    t30c[a,b,c,d,e] = ff3[a,f,g,h]*gg2[f,b,e]*gg1[g,d]*gg1[h,c]
+    t30d[a,b,c,d,e] = ff3[a,f,g,h]*gg2[f,c,d]*gg1[g,b]*gg1[h,e]
+    t30e[a,b,c,d,e] = ff3[a,f,g,h]*gg2[f,c,e]*gg1[g,d]*gg1[h,b]
+    t30f[a,b,c,d,e] = ff3[a,f,g,h]*gg2[f,d,e]*gg1[g,b]*gg1[h,c]
+    t40[a,b,c,d,e] = ff4[a,f,g,h,j]*gg1[f,b]*gg1[g,c]*gg1[h,d]*gg1[j,e]
+end
+t50 = t10 + t20 + t30 + t40
+t50 = reshape(t50,2,16)
+@test reshape(dfg4(x),2,16) ≈ t50
+
+import FaaDiBruno.Kronecker.a_mul_kron_b!
+work1 = zeros(n,n^4)
+work2 = zeros(n^5)
+a_mul_kron_b!(work1,ff[3],gg[[2,1,1]],work2)
+println("a_mul_kron_b!")
+println(work1[1])
+println("tensor product")
+println(t30a[1])
+
+dfg = zeros(n,n^4)
+FaaDiBruno.apply_recipees!(dfg,faadibruno_ws.recipees[4][3],ff[3],gg,4,faadibruno_ws)
+@test dfg ≈ reshape(t30,2,16)
+
+dfg = zeros(n,n^4)
+FaaDiBruno.apply_recipees!(dfg,faadibruno_ws.recipees[4][2],ff[2],gg,4,faadibruno_ws)
+@test dfg ≈ reshape(t20,2,16)
+
+dfg=zeros(n,n^3)
+FaaDiBruno.faa_di_bruno!(dfg,ff,gg,3,faadibruno_ws)
+target = reshape(dfg3(x),2,8)
+@test dfg ≈ target
+
+println("OK1")
+println(t10[1])
+println(t10[1]+t20[1])
+println(t10[1]+t20[1]+t30[1])
+println(t10[1]+t20[1]+t30[1]+t40[1])
+println("OK2")
+println([t30a[1], t30b[1], t30c[1], t30d[1], t30e[1], t30f[1]])
+dfg=zeros(n,n^4)
+FaaDiBruno.faa_di_bruno!(dfg,ff,gg,4,faadibruno_ws)
+target = reshape(dfg4(x),2,16)
+@test dfg ≈ target
