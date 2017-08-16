ea5c9bbae3b58c68f7423de479832a9d6e2ed97a
Michel Juillard <michel@michel-zen3.home>
2017-08-16 13:46:26 +0200

    simplified type declaration

--- src/solvers/gs_solver.jl
+++ src/solvers/gs_solver.jl
@@ -12,22 +12,17 @@ export GsSolverWS, gs_solver!
 type GsSolverWS
     dgges_ws::DggesWS
     linsolve_ws::LinSolveWS
-    D11::SubArray{Float64,2,Array{Float64,2},Tuple{UnitRange{Int64},UnitRange{Int64}},false}
-    E11::SubArray{Float64,2,Array{Float64,2},Tuple{UnitRange{Int64},UnitRange{Int64}},false}
-    Z11::SubArray{Float64,2,Array{Float64,2},Tuple{UnitRange{Int64},UnitRange{Int64}},false}
-    Z12::SubArray{Float64,2,Array{Float64,2},Tuple{UnitRange{Int64},UnitRange{Int64}},false}
-    Z21::SubArray{Float64,2,Array{Float64,2},Tuple{UnitRange{Int64},UnitRange{Int64}},false}
-    Z22::SubArray{Float64,2,Array{Float64,2},Tuple{UnitRange{Int64},UnitRange{Int64}},false}
-    tmp1::Array{Float64,2}
-    tmp2::Array{Float64,2}
-    tmp3::Array{Float64,2}
-    g1::Array{Float64,2}
-    g2::Array{Float64,2}
-#    gx_f::SubArray{Float64,2,Array{Float64,2},Tuple{UnitRange{Int64},Colon},false}
-#    hx::Array{Float64,2}
-#    ghx::Array{Float64,2}
-#    ghx_bb::SubArray{Float64,2,Array{Float64,2},Tuple{Array{Int64,1},Colon},false}
-#    ghx_f::SubArray{Float64,2,Array{Float64,2},Tuple{Array{Int64,1},Colon},false}
+    D11::SubArray{Float64}
+    E11::SubArray{Float64}
+    Z11::SubArray{Float64}
+    Z12::SubArray{Float64}
+    Z21::SubArray{Float64}
+    Z22::SubArray{Float64}
+    tmp1::Matrix{Float64}
+    tmp2::Matrix{Float64}
+    tmp3::Matrix{Float64}
+    g1::Matrix{Float64}
+    g2::Matrix{Float64}
 
     function GsSolverWS(d,e,n1)
         dgges_ws = DggesWS(Ref{UInt8}('N'),Ref{UInt8}('V'),e,d)
@@ -40,21 +35,17 @@ type GsSolverWS
         Z12 = view(dgges_ws.vsr,1:n1,n1+1:n)
         Z21 = view(dgges_ws.vsr,n1+1:n,1:n1)
         Z22 = view(dgges_ws.vsr,n1+1:n,n1+1:n)
-        tmp1 = Array(Float64,n2,n1)
-        tmp2 = Array(Float64,n1,n1)
-        tmp3 = Array(Float64,n1,n1)
-        g1 = Array(Float64,n1,n1)
-        g2 = Array(Float64,n2,n1)
-#        gx_f = view(gx,1:n2,:)
-#        ghx = Array(Float64,model.endo_nbr,n1)
-#        ghx_bb = view(ghx,i1,:)
-#        ghx_f = view(ghx,i2,:)
+        tmp1 = Matrix{Float64}(n2,n1)
+        tmp2 = Matrix{Float64}(n1,n1)
+        tmp3 = Matrix{Float64}(n1,n1)
+        g1 = Matrix{Float64}(n1,n1)
+        g2 = Matrix{Float64}(n2,n1)
         new(dgges_ws,linsolve_ws,D11,E11,Z11,Z12,Z21,Z22,tmp1,tmp2,tmp3,g1,g2)
     end
 end
 
 """
-    gs_solver!(ws::GsSolverWS,d::Array{Float64,2},e::Array{Float64,2},n1::Int64,qz_criterium)
+    gs_solver!(ws::GsSolverWS,d::Matrix{Float64},e::Matrix{Float64},n1::Int64,qz_criterium)
 
 finds the unique stable solution for the following system:
 
@@ -62,7 +53,7 @@ finds the unique stable solution for the following system:
 d \left[\begin{array}{c}I\\g_2\end{array}\right]g_1 = e \left[\begin{array}{c}I\\g_2\end{array}\right]
 ```
 """
-function gs_solver!(ws::GsSolverWS,d::Array{Float64,2},e::Array{Float64,2},n1::Int64,qz_criterium::Float64)
+function gs_solver!(ws::GsSolverWS,d::Matrix{Float64},e::Matrix{Float64},n1::Int64,qz_criterium::Float64)
 
     dgges!(ws.dgges_ws,e,d)
     nstable = ws.dgges_ws.sdim[]
