01538c4986c500252519386df1fac8627210fb9b
Michel Juillard <michel@michel-zen3.home>
2017-11-30 16:00:50 +0100

    adding function to solve linear system with precomputed LU

--- src/linalg/linsolve_algo.jl
+++ src/linalg/linsolve_algo.jl
@@ -5,7 +5,7 @@ import Base.LinAlg.BLAS.@blasfunc
 import Base.LinAlg.BLAS.libblas
 import Base.LinAlg.LAPACK: liblapack, chklapackerror
 
-export LinSolveWS, linsolve_core!
+export LinSolveWS, linsolve_core!, linsolve_core_no_lu!, lu!
 
 struct LinSolveWS
     lu::Matrix{Float64}
@@ -15,6 +15,25 @@ struct LinSolveWS
         lu = Matrix{Float64}(n,n)
         ipiv = Vector{BlasInt}(n)
         new(lu,ipiv)
+    end
+end
+
+function linsolve_core_no_lu!(ws::LinSolveWS,trans::Ref{UInt8},a::StridedMatrix{Float64},b::StridedVecOrMat{Float64})
+    mm,nn = size(a)
+    m = Ref{BlasInt}(mm)
+    n = Ref{BlasInt}(nn)
+    nhrs = Ref{BlasInt}(size(b,2))
+    lda = Ref{BlasInt}(max(1,stride(a,2)))
+    ldb = Ref{BlasInt}(max(1,stride(b,2)))
+    info = Ref{BlasInt}(0)
+
+    ccall((@blasfunc(dgetrs_), liblapack), Void,
+          (Ref{UInt8},Ref{BlasInt},Ref{BlasInt},Ptr{Float64},Ref{BlasInt},
+           Ptr{BlasInt},Ptr{Float64},Ref{BlasInt},Ref{BlasInt}),
+          trans,n,nhrs,ws.lu,lda,ws.ipiv,b,ldb,info)
+    if info[] != 0
+        println("dgetrs ",info[])
+        chklapackerror(info[])
     end
 end
 
