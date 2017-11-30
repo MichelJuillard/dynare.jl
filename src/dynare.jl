9b4033fdf60e8772d0cfd2c7c545dcb506bebf59
Michel Juillard <michel@michel-zen3.home>
2017-11-30 15:58:18 +0100

    updating modules

--- src/dynare.jl
+++ src/dynare.jl
@@ -1,14 +1,28 @@
 module Dynare
 
 include("linalg/linalg.jl")
+using .DynLinAlg
 include("model.jl")
 using .model
 export Model, get_de, get_abc
+include("taylor/faadibruno.jl")
 include("solvers/solvers.jl")
+using .Solvers.ResultsPerturbationWs
+export ResultsPerturbationWs
 using .Solvers.FirstOrder
 export FirstOrderSolverWS, first_order_solver
 using .Solvers.GeneralizedSylvester
 export EyePlusAtKronBWS, general_sylvester_solver!
-using .Solvers.SecondOrder
-export SecondOrderSolverWS, second_order_solver!
+using .Solvers.KOrder
+export KOrderWs, k_order_solution!
+include("models/DynareModel.jl")
+using DynareModel
+include("models/DynareOptions.jl")
+using DynareOptions
+include("models/DynareOutput.jl")
+using DynareOutput
+include("models/SteadyState.jl")
+using SteadyState
+include("models/Utils.jl")
+using Utils
 end
